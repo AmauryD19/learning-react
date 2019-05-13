3c24da6b9feaf75fa0942ade910b330c18e965c7
AmauryD19 <amaurydotrice@gmail.com>
2019-05-13 09:14:41 +0200

    Happy Drink first

---
+++ one-page/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Hello from '../hello/Hello'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+            </div>
+        );
+    }
+}
