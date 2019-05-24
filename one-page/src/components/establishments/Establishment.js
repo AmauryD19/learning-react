b5c288753f17eb034028630227177a29dd9ea9e9
AmauryD19 <amaurydotrice@gmail.com>
2019-05-24 16:10:45 +0200

    finally understood what was wrong.
    After looking for internship and working on client project I saw the light and ran into this repo

--- one-page/src/components/establishments/Establishment.js
+++ one-page/src/components/establishments/Establishment.js
@@ -1,83 +1,88 @@
 import React from 'react';
 
-
 class Establishment extends React.Component {
+	constructor(props) {
+		super(props);
 
-    constructor(props) {
+		this.state = {
+			isLiked: false,
+			isDisliked: false,
+			likeCounter: 0,
+			dislikeCounter: 0,
+			favori: false
+		};
+	}
+	like = () => {
+		let likeCounter = this.state.likeCounter;
+		let dislikeCounter = this.state.dislikeCounter;
 
-        super(props);
+		likeCounter += !this.state.isLiked ? 1 : -1;
+		dislikeCounter += this.state.isDisliked ? -1 : 0;
 
-        this.state = {
-            isLiked : false,
-            isDisliked : false,
-            likeCounter: 0,
-            dislikeCounter : 0
-        }
-    }
+		this.setState({
+			isLiked: !this.state.isLiked,
+			isDisliked: this.state.isDisliked ? !this.state.isDisliked : this.state.isDisliked,
+			likeCounter: likeCounter,
+			dislikeCounter: dislikeCounter
+		});
+	};
 
-    like = () =>{
+	dislike = () => {
+		let likeCounter = this.state.likeCounter;
+		let dislikeCounter = this.state.dislikeCounter;
 
-        let likeCounter = this.state.likeCounter;
-        let dislikeCounter = this.state.dislikeCounter;
+		dislikeCounter += !this.state.isDisliked ? 1 : -1;
+		likeCounter += this.state.isLiked ? -1 : 0;
 
-        likeCounter += !this.state.isLiked ? 1 : -1
-        dislikeCounter  +=  this.state.isDisliked ? -1 : 0
+		this.setState({
+			isDisliked: !this.state.isDisliked,
+			isLiked: this.state.isLiked ? !this.state.isLiked : this.state.isLiked,
+			likeCounter: likeCounter,
+			dislikeCounter: dislikeCounter
+		});
+	};
 
-        this.setState({
-            isLiked : !this.state.isLiked,
-            isDisliked : this.state.isDisliked ? !this.state.isDisliked : this.state.isDisliked,
-            likeCounter : likeCounter,
-            dislikeCounter : dislikeCounter
-        })
-    }
+	favori = () => {
+		this.setState({
+			favori: !this.state.favori
+		});
+	};
+	render() {
+		// On définit les éléments "upIcon(like) et downIcon(dislike)"
+		// on utilise ici Font-Answome (on inclura le fichier css dans ./public.index.html)
+		let upIcon = <i className="fa fa-thumbs-up" aria-hidden="true" />;
+		let downIcon = <i className="fa fa-thumbs-down" aria-hidden="true" />;
+		let starIcon = <i className="fa fa-star-o" aria-hidden="true" />;
 
-    dislike = () =>{
-        let likeCounter = this.state.likeCounter;
-    let dislikeCounter = this.state.dislikeCounter;
+		// Si l'on n'a pas encore "liké"
+		if (!this.state.isLiked) {
+			upIcon = <i className="fa fa-thumbs-o-up" aria-hidden="true" />;
+		}
+		// Si l'on a pas encore "disliké"
+		if (!this.state.isDisliked) {
+			downIcon = <i className="fa fa-thumbs-o-down" aria-hidden="true" />;
+		}
+		if (this.state.favori) {
+			starIcon = <i className="fa fa-star favoriIcon" aria-hidden="true" />;
+		}
 
-    dislikeCounter +=  !this.state.isDisliked ? 1 : -1
-    likeCounter +=  this.state.isLiked ? -1 : 0
+		return (
+			<div className="establishment">
+				<div className="establishment-favori">
+					<button onClick={this.favori}>{starIcon}</button>
+				</div>
+				<div className="establishment-description">
+					<h3>{this.props.establishment.name}</h3>
 
-    this.setState({
-        isDisliked : !this.state.isDisliked,
-        isLiked : this.state.isLiked ? !this.state.isLiked : this.state.isLiked,
-        likeCounter : likeCounter,
-        dislikeCounter : dislikeCounter
-        })
-    }
-    
-    render() {
-
-        // On définit les éléments "upIcon(like) et downIcon(dislike)"
-        // on utilise ici Font-Answome (on inclura le fichier css dans ./public.index.html)
-        let upIcon = <i className="fa fa-thumbs-up" aria-hidden="true"></i>
-        let downIcon = <i className="fa fa-thumbs-down" aria-hidden="true"></i>
-
-        // Si l'on n'a pas encore "liké"
-        if(!this.state.isLiked){
-            upIcon = <i className="fa fa-thumbs-o-up" aria-hidden="true"></i>
-        }
-        // Si l'on a pas encore "disliké"
-        if(!this.state.isDisliked){
-            downIcon = <i className="fa fa-thumbs-o-down" aria-hidden="true"></i>
-        }
-
-        return (
-            <div className='establishment' >
-                <div className='establishment-description' >
-                    <h3>{ this.props.establishment.name }</h3>
-
-                    { this.props.establishment.description }
-                </div>
-                <div className='establishmentLikeDislike' >
-                    {/* Au clic sur le bouton on appelle la fonction */}
-                    <button onClick={this.like}>{ upIcon } </button> <span>{ this.state.likeCounter }</span>
-                    <button onClick={this.dislike}>{ downIcon }</button> <span>{ this.state.dislikeCounter }</span>
-                </div>
-
-            </div>
-        );
-    }
+					{this.props.establishment.description}
+				</div>
+				<div className="establishmentLikeDislike">
+					<button onClick={this.like}>{upIcon} </button> <span>{this.state.likeCounter}</span>
+					<button onClick={this.dislike}>{downIcon}</button> <span>{this.state.dislikeCounter}</span>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Establishment;
