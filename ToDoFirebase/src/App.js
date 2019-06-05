e01872a2cb05ef7746f4f03c3491b1f7bd2ef5c2
AmauryD19 <amaurydotrice@gmail.com>
2019-06-05 10:46:11 +0200

    oui

--- ToDoFirebase/src/App.js
+++ ToDoFirebase/src/App.js
@@ -7,81 +7,79 @@ import 'firebase/database';
 import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.addNote = this.addNote.bind(this);
+		this.removeNote = this.removeNote.bind(this);
 
-  constructor(props){
-    super(props);
-    this.addNote = this.addNote.bind(this);
-    this.removeNote = this.removeNote.bind(this);
+		this.app = firebase.initializeApp(DB_CONFIG);
+		this.database = this.app.database().ref().child('notes');
 
-    this.app = firebase.initializeApp(DB_CONFIG);
-    this.database = this.app.database().ref().child('notes');
+		// Setup React state of component
+		this.state = {
+			notes: []
+		};
+	}
 
-    // Setup React state of component
-    this.state = {
-      notes: [],
-    }
-  }
+	componentWillMount() {
+		const previousNotes = this.state.notes;
 
-  componentWillMount() {
-    const previousNotes = this.state.notes;
+		// Snapshot data
+		this.database.on('child_added', (snap) => {
+			previousNotes.push({
+				id: snap.key,
+				noteContent: snap.val().noteContent
+			});
 
-    // Snapshot data
-    this.database.on('child_added', snap => {
-      previousNotes.push({
-        id: snap.key,
-        noteContent: snap.val().noteContent,
-      })
+			this.setState({
+				notes: previousNotes
+			});
+		});
 
-      this.setState({
-        notes: previousNotes
-      })
-    })
-  
+		this.database.on('child_removed', (snap) => {
+			for (let i = 0; i < previousNotes.length; i++) {
+				if (previousNotes[i].id === snap.key) {
+					previousNotes.splice(i, 1);
+				}
+			}
 
-    this.database.on('child_removed', snap => {
-      for(let i=0; i < previousNotes.length; i++){
-        if(previousNotes[i].id === snap.key){
-          previousNotes.splice(i, 1);
-        }
-      } 
+			this.setState({
+				notes: previousNotes
+			});
+		});
+	}
+	addNote(note) {
+		this.database.push().set({ noteContent: note });
+	}
 
-      this.setState({
-        notes: previousNotes
-      })
-    })
-  }
-  addNote(note){
-    this.database.push().set({ noteContent: note});
-  }
+	removeNote(noteId) {
+		this.database.child(noteId).remove();
+	}
 
-  removeNote(noteId){
-    this.database.child(noteId).remove();
-  }
-
-  render() {
-    return (
-      <div className="notesWrapper">
-        <div className="notesHeader">
-          <div className="heading">To Do List</div>
-        </div>
-        <div className="notesBody">
-          {
-            this.state.notes.map((note) => {
-              return (
-                <Note noteContent={note.noteContent} 
-                noteId={note.id} 
-                key={note.id} 
-                removeNote={this.removeNote}/>
-              )
-            })
-          }
-          </div>
-        <div className="notesFooter">
-          <NoteForm addNote={this.addNote}/>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="notesWrapper">
+				<div className="notesHeader">
+					<div className="heading">To Do List</div>
+				</div>
+				<div className="notesBody">
+					{this.state.notes.map((note) => {
+						return (
+							<Note
+								noteContent={note.noteContent}
+								noteId={note.id}
+								key={note.id}
+								removeNote={this.removeNote}
+							/>
+						);
+					})}
+				</div>
+				<div className="notesFooter">
+					<NoteForm addNote={this.addNote} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- ToDoFirebase/src/NoteForm/NoteForm.jsx
+++ ToDoFirebase/src/NoteForm/NoteForm.jsx
@@ -1,45 +1,48 @@
 import React, { Component } from 'react';
 import './NoteForm.css';
 
-class NoteForm extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            newNoteContent: '',
-        };
-        this.handleUserInput = this.handleUserInput.bind(this);
-        this.writeNote = this.writeNote.bind(this);
+class NoteForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			newNoteContent: ''
+		};
+		this.handleUserInput = this.handleUserInput.bind(this);
+		this.writeNote = this.writeNote.bind(this);
+	}
 
-    }
+	//When user's input changes, set newNoteContent
+	//to the value in the input box
+	handleUserInput(e) {
+		this.setState({
+			newNoteContent: e.target.value //Value of text's inpput
+		});
+	}
 
-    //When user's input changes, set newNoteContent
-    //to the value in the input box
-    handleUserInput(e){
-        this.setState({
-            newNoteContent: e.target.value, //Value of text's inpput
-        })
-    }
+	writeNote() {
+		//Set noteContent for a value of inputs
+		this.props.addNote(this.state.newNoteContent);
+		//set newNoteContent to an empty string
+		this.setState({
+			newNoteContent: ''
+		});
+	}
 
-    writeNote(){
-        //Set noteContent for a value of inputs
-        this.props.addNote(this.state.newNoteContent);
-        //set newNoteContent to an empty string
-        this.setState({
-            newNoteContent: '',
-        })
-    }
-
-    render() {
-        return(
-            <div className="formWrapper">
-                <input className="noteInput" placeholder="Jerrome, par ici..." 
-                    value={this.state.newNoteContent}
-                    onChange={this.handleUserInput}/>
-                <button className="noteButton"
-                onClick={this.writeNote}>Add Note</button>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="formWrapper">
+				<input
+					className="noteInput"
+					placeholder="Jerrome, par ici..."
+					value={this.state.newNoteContent}
+					onChange={this.handleUserInput}
+				/>
+				<button className="noteButton" onClick={this.writeNote}>
+					Add Note
+				</button>
+			</div>
+		);
+	}
 }
 
 export default NoteForm;
